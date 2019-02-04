f8a933144dc8bc7e1d9e20b354fb77b98a747932
jeff15113 <itsmejeffery@gmail.com>
2019-02-04 17:02:04 -0500

    Created register form presentational component

--- src/components/Register/registerForm.js
+++ src/components/Register/registerForm.js
@@ -3,15 +3,23 @@ import React from "react";
 const registerForm = props => {
   return (
     <div>
-      <form onSubmit={props.register} method="post">
-        <input type="text" name="username" value="" placeholder="Username" />
+      <form onSubmit={props.handleSubmit} method="post">
         <input
+          onChange={props.handleChange}
+          type="text"
+          name="username"
+          value={props.username}
+          placeholder="Username"
+        />
+        <input
+          onChange={props.handleChange}
           type="password"
           name="password"
           id="password"
-          value=""
+          value={props.password}
           placeholder="Password"
         />
+        <button type="submit">Register</button>
       </form>
     </div>
   );
