06bc75657dbbb69189355fb08afd05f53c44324d
jeff15113 <itsmejeffery@gmail.com>
2019-02-04 20:37:13 -0500

    added conditional auth component render

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
+import Auth from "./components/Auth";
+import { connect } from "react-redux";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
-        <Register />
+        {!this.props.loggedin ? <Auth /> : <h1> Welcome </h1>}
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    loggedin: state.isLoggedIn
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  {}
+)(App);
